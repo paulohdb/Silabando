26bcb0d2c7aa3ece040c8f9f2645b01dd28064b6
Elton Naoki Iwakura <eltoniwakura@tce.ms.gov.br>
2020-10-19 18:15:50 -0400

    ajustes layout

---
+++ Assets/Scripts/Gato.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Gato : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private List<string> ordemCorreta = new List<string> {
+        "GA",
+        "TO"
+    };
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void Ga(Button button)
+    {
+        if (ordemCorreta.First() == "GA")
+        {
+            var colors = button.colors;
+            colors.normalColor = Color.green;
+            colors.highlightedColor = Color.green;
+            colors.pressedColor = Color.green;
+            colors.selectedColor = Color.green;
+            colors.disabledColor = Color.green;
+            button.colors = colors;
+
+            ordemCorreta.Remove("GA");
+        }
+        else
+        {
+            var colors = button.colors;
+            colors.normalColor = Color.red;
+            colors.highlightedColor = Color.red;
+            colors.pressedColor = Color.red;
+            colors.selectedColor = Color.red;
+            colors.disabledColor = Color.red;
+            button.colors = colors;
+
+            ordemCorreta = new List<string> {
+                "GA",
+                "TO"
+            };
+        }
+    }
+
+
+    public void To(Button button)
+    {
+        Debug.Log("cliquei no TO!!!");
+
+        if (ordemCorreta.First() == "TO")
+        {
+            var colors = button.colors;
+            colors.normalColor = Color.green;
+            colors.highlightedColor = Color.green;
+            colors.pressedColor = Color.green;
+            colors.selectedColor = Color.green;
+            colors.disabledColor = Color.green;
+            button.colors = colors;
+
+            ordemCorreta.Remove("TO");
+            //Apresentar para trocar de nivel
+
+
+        }
+        else
+        {
+            var colors = button.colors;
+            colors.normalColor = Color.red;
+            colors.highlightedColor = Color.red;
+            colors.pressedColor = Color.red;
+            colors.selectedColor = Color.red;
+            colors.disabledColor = Color.red;
+            button.colors = colors;
+
+            ordemCorreta = new List<string> {
+                "GA",
+                "TO"
+            };
+        }
+    }
+
+
+
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
