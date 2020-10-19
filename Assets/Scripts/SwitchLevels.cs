ec915c714a353b467c6ed0793f487061642b83a6
P2500 <paulinhogt6@gmail.com>
2020-10-19 16:07:50 -0400

    first commit

---
+++ Assets/Scripts/SwitchLevels.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevels : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
