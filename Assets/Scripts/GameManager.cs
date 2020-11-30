b410f2c8fd0dda1df12b9ec1a3bbca40a043913a
MarquesMan <marquesnavarezi@hotmail.com>
2020-11-30 12:00:26 -0400

    Adicionando arquivos a mais

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,17 +11,49 @@ public class GameManager : MonoBehaviour
 
     public List<GameObject> ordemCorreta;
     public static GameManager Instance;
-    private bool ganhou;
     public GameObject imagemGanhou;
+    public Text silabasText;
+
+    private bool ganhou;
+    private int silabasStringOffset = 0;
+    private StringBuilder silabasString;
+
+    public void Start()
+    {
+        silabasString = new StringBuilder("");
+
+        if (silabasText)
+        {
+            foreach(GameObject button in ordemCorreta)
+            {
+                silabasString.Append('_', button.GetComponentInChildren<Text>().text.Length);
+                silabasString.Append('-');
+            }
+
+        }
+        silabasString.Remove(silabasString.Length - 1, 1);
+        silabasText.text = silabasString.ToString();
+    }
+
+    private void addSilaba(string silaba)
+    {
+
+        silabasString.Replace(silabasString.ToString(silabasStringOffset, silaba.Length), silaba, silabasStringOffset, silaba.Length);
+        silabasStringOffset += silaba.Length + 1;
+        silabasText.text = silabasString.ToString();
+    }
 
     public void Verifica(GameObject buttonGameObject)
     {
         if(ganhou)
             return;
+
+        var button = buttonGameObject.GetComponent<Button>();
+        var colors = button.colors;
+
         if (ordemCorreta.First().Equals(buttonGameObject))
         {
-            var button = buttonGameObject.GetComponent<Button>();
-            var colors = button.colors;
+            
             colors.normalColor = Color.green;
             colors.highlightedColor = Color.green;
             colors.pressedColor = Color.green;
@@ -30,14 +63,14 @@ public class GameManager : MonoBehaviour
             button.interactable = false;
             ordemCorreta.RemoveAt(0);
             ganhou = ordemCorreta.Count == 0;
-            if(imagemGanhou != null)
+            addSilaba(button.GetComponentInChildren<Text>().text);
+
+            if (imagemGanhou != null)
                 imagemGanhou.SetActive(ganhou);
         }
 
         else
         {
-            var button = buttonGameObject.GetComponent<Button>();
-            var colors = button.colors;
             StartCoroutine(Release(button, colors));
             colors.normalColor = Color.red;
             colors.highlightedColor = Color.red;
@@ -45,15 +78,14 @@ public class GameManager : MonoBehaviour
             colors.selectedColor = Color.red;
             colors.disabledColor = Color.red;
             button.colors = colors;
-            
-
-
+            button.interactable = false;
         }
     }
 
     private IEnumerator Release(Button button, ColorBlock colors)
     {
         yield return new WaitForSeconds(1f);
+        button.interactable = true;
         button.colors = colors;
     }
     
